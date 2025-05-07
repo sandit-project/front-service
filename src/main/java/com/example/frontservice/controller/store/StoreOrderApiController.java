1995c39027455312afaa87c0aea0785c6928b806
elec1001 <elec1001@gmail.com>
2025-05-07 18:22:28 +0900

    250507

--- src/main/java/com/example/frontservice/controller/store/StoreOrderApiController.java
+++ src/main/java/com/example/frontservice/controller/store/StoreOrderApiController.java
@@ -2,13 +2,17 @@ package com.example.frontservice.controller.store;
 
 import com.example.frontservice.dto.store.StoreOrderCountResponseDTO;
 import com.example.frontservice.dto.store.StoreOrderListResponseDTO;
+import com.example.frontservice.dto.store.StoreOrderResponseDTO;
 import com.example.frontservice.service.StoreOrderService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,14 +21,15 @@ public class StoreOrderApiController {
     private final StoreOrderService storeOrderService;
 
     @GetMapping("/orders/store/{storeUid}")
-    public StoreOrderListResponseDTO getAllOrdersByStoreUid(  @PathVariable(name = "storeUid") Integer storeUid,
-                                                              @RequestParam(name = "limit", defaultValue = "10") int limit,
-                                                              @RequestParam(name = "lastUid", required = false) Integer lastUid,
-                                                              @RequestParam(name = "status", required = false) String status,
-                                                              HttpServletRequest request) {
+    public ResponseEntity<List<StoreOrderResponseDTO>> getAllOrdersByStoreUidAndStatus(
+                                                        @PathVariable(name = "storeUid") Integer storeUid,
+                                                        @RequestParam(name = "status", required = false) String status,
+                                                        HttpServletRequest request) {
         String token = request.getHeader("Authorization");
 
-        return storeOrderService.getOrdersByStoreUid(storeUid,limit,lastUid,status,token);
+        List<StoreOrderResponseDTO> storeOrderResponseDTOList = storeOrderService.getOrdersByStoreUidAndStatus(storeUid,status,token);
+
+        return ResponseEntity.ok(storeOrderResponseDTOList);
     }
 
     @GetMapping("orders/store/{storeUid}/count")
