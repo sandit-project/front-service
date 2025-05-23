4b4990117829f81160cd4c38637a5f013bb020ab
hankookkim <nahmik84@gmail.com>
2025-05-23 15:24:16 +0900

    5/23

--- src/main/java/com/example/frontservice/controller/menu/MenuCustomerListViewController.java
+++ src/main/java/com/example/frontservice/controller/menu/MenuCustomerListViewController.java
@@ -1,18 +1,25 @@
 package com.example.frontservice.controller.menu;
 
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 
 public class MenuCustomerListViewController {
+    @Value("${app.websocket.url}")
+    private String websocketUrl;
 
 
 
     // 고객용 메뉴 목록 페이지
     @GetMapping("/")
-    public String showCustomerMenuList() {
+    public String showCustomerMenuList(Model model) {
+
+        model.addAttribute("websocketUrl", websocketUrl);
         return "menu/menuCustomerList"; // Model 제거
     }
 }
