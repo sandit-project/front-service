99bc7f666afb5de734e14799993d1e1dc16d9608
hankookkim <nahmik84@gmail.com>
2025-05-29 18:00:25 +0900

    5/29

--- src/main/java/com/example/frontservice/controller/menu/SideMenuForCustomerViewController.java
+++ src/main/java/com/example/frontservice/controller/menu/SideMenuForCustomerViewController.java
@@ -1,17 +1,21 @@
 package com.example.frontservice.controller.menu;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class SideMenuForCustomerViewController {
-
+    @Value("${app.websocket.url}")
+    private String websocketUrl;
 
 
     @GetMapping("/sidesInfo/{sideName}")
-    public String showMenuDetail(@PathVariable String sideName) {
+    public String showMenuDetail(@PathVariable String sideName, Model model) {
+        model.addAttribute("websocketUrl", websocketUrl);
         return "menu/sideMenuForCustomer"; // 화면만 반환 (HTML 처리)
     }
 
