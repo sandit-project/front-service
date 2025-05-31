cccd5ec4b339b45d2da5caf85a4596471bd0e91f
rangrang-53 <jr0503@naver.com>
2025-05-31 22:26:14 +0900

    update

--- src/main/java/com/example/frontservice/config/RestTemplateConfig.java
+++ src/main/java/com/example/frontservice/config/RestTemplateConfig.java
@@ -3,13 +3,21 @@ package com.example.frontservice.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        // 오직 JSON 컨버터만 등록
+        restTemplate.setMessageConverters(
+                Collections.singletonList(new MappingJackson2HttpMessageConverter())
+        );
+        return restTemplate;
     }
 }
