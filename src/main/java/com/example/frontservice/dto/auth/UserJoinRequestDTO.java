606644fe8716c5476e163db8f7bb583cae4e5d1f
dlstlr1707 <dlstlr1707@naver.com>
2025-06-01 00:33:42 +0900

    moify code

--- src/main/java/com/example/frontservice/dto/auth/UserJoinRequestDTO.java
+++ src/main/java/com/example/frontservice/dto/auth/UserJoinRequestDTO.java
@@ -1,11 +1,15 @@
 package com.example.frontservice.dto.auth;
 
 import com.example.frontservice.type.Role;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserJoinRequestDTO {
     private String userId;
     private String password;
